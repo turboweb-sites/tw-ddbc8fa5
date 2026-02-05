19eb5e9880e0c3394824f37e3f6b816efb0b90ca
Zhanna Kutuzova <zhannamichail@gmail.com>
2026-02-05 14:14:20 -0800

    Update from TurboWeb

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT || '3000'),
-    allowedHosts: 'all',
-  },
-  preview: {
-    host: '0.0.0.0',
-    allowedHosts: 'all',
   },
 })
